f284b49be9fcfd57b6309d53d6baddf225f7b557
m83880 <jsin@tdc.dk>
2018-04-24 15:16:26 +0200

    ---------

--- roles/Juniper.junos/callback_plugins/jsnapyfull.py
+++ roles/Juniper.junos/callback_plugins/jsnapyfull.py
@@ -37,7 +37,7 @@ class CallbackModule(CallbackBase):
     module_name = ''
     module_args = {}
     #self._display.display(str(result._result))
-    
+    self._display.display("2------------------------------------------------------------------------------------------------------------------------")    
     if 'invocation' in result._result:
       if 'module_name' in result._result['invocation']:
         module_name = result._result['invocation']['module_name']
